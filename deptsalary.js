1ef2679546a21821a49b197cc6b436c09b823816
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-02-22 01:11:40 +0000

    added load_dept_emp.txt and removed INSERT text

--- deptsalary.js
+++ deptsalary.js
@@ -4,11 +4,11 @@ var fs = require("fs");
 // push single string data/elements into an array as a single element
 //push array data into an array to form muli-d arrays
 
-//single-d arrays
+// single-d arrays --valid
 var departmentId = [];
 var departments = [];
 
-//multi-d arrays
+//multi-d arrays -- invalid
 var employeeId = [];
 var employeeName = [];
 var salaries = [];
@@ -19,14 +19,37 @@ fs.readFile("load_dept_names.txt", "utf8", function(err, data){
 
 	
 	var deptDataClean = data.replace(/INSERT INTO `departments` VALUES \n/g, "");
-	
 	var deptDataArray = deptDataClean.split("\n");
+	
+	
 	for (var i = 0; i < deptDataArray.length; i++) {
+		//populate single-d arrays with DATA
 		departmentId.push(deptDataArray[i].slice(2,6));
+		departments.push(deptDataArray[i].slice(9,-3));
+		
+		employeeId.push([]);
+		employeeName.push([]);
+		salaries.push([]);
 	}
-		for (var j = 0; j < deptDataArray.length; j++) {
-		departments.push(deptDataArray[j].slice(9,-3));
-	}
+		// populate multi-d arrays with empty sub-arrays (NO DATA!!!)
+		
+		
+	
 	console.log(departmentId);
 	console.log(departments);
-})
+	console.log(employeeId);
+	console.log(employeeName);
+	console.log(salaries);
+	
+});
+
+// Process "load_dept_emp.txt" file
+fs.readFile("load_dept_emp.txt", "utf8", function(err, data){
+	if (err) throw err;
+	
+	var employeeDataClean = data.replace(/INSERT INTO `dept_emp` VALUES /g, "");
+	var employeeDataArray = employeeDataClean.split("\n");
+	
+	console.log(employeeDataArray);
+	
+});

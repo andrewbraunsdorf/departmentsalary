6880abc308bbd06706217b5b3d285b8cced24208
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-02-23 17:00:36 +0000

    split salary lines by active salaries

--- deptsalary.js
+++ deptsalary.js
@@ -14,52 +14,114 @@ var employeeName = [];
 var salaries = [];
 
 // Process "load_dept_name.txt" file
-fs.readFile("load_dept_names.txt", "utf8", function(err, data){
+fs.readFile("load_dept_names.txt", "utf8", function(err, data) {
 	if (err) throw err;
 
-	
+
 	var deptDataClean = data.replace(/INSERT INTO `departments` VALUES \n/g, "");
 	var deptDataArray = deptDataClean.split("\n");
-	
-	
+
+
 	for (var i = 0; i < deptDataArray.length; i++) {
 		//populate single-d arrays with DATA
-		departmentId.push(deptDataArray[i].slice(2,6));
-		departments.push(deptDataArray[i].slice(9,-3));
-		
+		departmentId.push(deptDataArray[i].slice(2, 6));
+		departments.push(deptDataArray[i].slice(9, -3));
+
 		employeeId.push([]);
 		employeeName.push([]);
 		salaries.push([]);
 	}
-		// populate multi-d arrays with empty sub-arrays (NO DATA!!!)
-		
-		
-	
-	console.log(departmentId);
-	console.log(departments);
-	console.log(employeeId);
-	console.log(employeeName);
-	console.log(salaries);
-	
+	// populate multi-d arrays with empty sub-arrays (NO DATA!!!)
+
+
+
+	// console.log(departmentId);
+	// console.log(departments);
+	// console.log(employeeId);
+	// console.log(employeeName);
+	// console.log(salaries);
+
 });
 
 // Process "load_dept_emp.txt" file
-fs.readFile("load_dept_emp.txt", "utf8", function(err, data){
+fs.readFile("load_dept_emp.txt", "utf8", function(err, data) {
 	if (err) throw err;
-	
+
 	var employeeDataClean = data.replace(/INSERT INTO `dept_emp` VALUES /g, "");
 	var employeeDataArray = employeeDataClean.split("\n");
-	
+
 	for (var i = 0; i < employeeDataArray.length; i++) {
 		if (employeeDataArray[i].slice(28, 32) == "9999") {
-			
+
 			// console.log(employeeDataArray[i].slice(8, 12));
 			// console.log(employeeDataArray[i].slice(1, 6));
-			employeeId[departmentId.indexOf(employeeDataArray[i].slice(8, 12))].push(employeeDataArray[i].slice(1,6));
+			employeeId[departmentId.indexOf(employeeDataArray[i].slice(8, 12))].push(employeeDataArray[i].slice(1, 6));
 		}
 	}
-//	employeeId.push(employeeDataArray[i])
-	
+	//	employeeId.push(employeeDataArray[i])
+
 	console.log(employeeId);
-	
+
 });
+
+// Process "load_salaries1.txt" file
+
+
+fs.readFile("load_salaries1.txt", "utf8", function(err, data) {
+	if (err) throw err;
+
+	var salaryDataClean = data.replace(/INSERT INTO `salaries` VALUES /g, "");
+	var salaryDataArray = salaryDataClean.split("\n");
+
+	console.log(salaryDataArray[0].slice(27, 31));
+
+	// for (var i = 0; i < salaryDataArray.length; i++) {
+	// 	if (salaryDataArray[i].slice(27, 31) == "9999") {
+			
+
+
+	// 		console.log(salaryDataArray[i].slice(1, 6));
+
+	// 	}
+	// 	else {
+	// 		console.log(salaryDataArray[i].slice(28, 32));
+	// 	}
+	// }
+	// console.log(salaries);
+
+});
+// for (var j = 0; j < employeeId[i].length; j++) {
+// employeeId[departmentId.indexOf(employeeDataArray[i].slice(8, 12))].push(employeeDataArray[i].slice(1,6));
+// salaries[employeeId[i].indexOf(salaryDataArray[i][j].slice(2, 6))].push(salaryDataArray[i].slice(7, 11));
+// }
+
+
+
+
+
+// .split(",").reverse().join("")
+
+// fs.readFile("load_employee.txt", "utf8", function(err, data){
+// 	if (err) throw err;
+
+// 	var employeeNameDataClean = data.replace(/INSERT INTO `employees` VALUES /g, "");
+// 	var employeeNameDataArray = employeeNameDataClean.split("\n");
+
+// 	for (var i = 0; i < departments.length; i++) {
+// 		for (var j = 0; j < employeeId[i].length; j++) {
+// 		// console.log(employeeNameDataArray[i].slice(21, -20));
+// 		// employeeId[departmentId.indexOf(employeeDataArray[i].slice(8, 12))].push(employeeDataArray[i].slice(1,6));
+// 		employeeName[employeeId.indexOf(employeeNameDataArray[i][j].slice(2, 6))].push(employeeNameDataArray[i][j].slice(20, -19));
+// 	}
+// 	}
+// 	console.log(employeeName);
+
+// });
+
+
+//dump all fs files into one file.AS
+
+// replaced () to []
+// replace '' with "" using regex. using .replace()
+
+// use .json file parse to create an
